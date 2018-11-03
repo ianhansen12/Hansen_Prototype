4ee65f23b291cfdb553cd9a3703c8f0e6d1672f0
Hagrid <scu.imaginarium@gmail.com>
2018-11-02 18:02:29 -0700

    Prototype

---
+++ Assets/GrabFlower.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabFlower : MonoBehaviour
+{
+    public GameObject flower1;
+    public GameObject flower2;
+    public GameObject flower3;
+    private GameObject touchedFlower;
+    private bool cash = false;
+    public Transform anchor;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    //GameObject instantiatedobject = Instantiate(prefab) as GameObject;
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            if(cash == true)
+            {
+                gettea();
+            }
+        }
+    }
+    void OnTriggerEnter(Collider col)
+    {
+            if (col.CompareTag("Chamomile"))
+            {
+            //flower = col.gameObject;
+                touchedFlower = flower1;
+                cash = true;
+            }
+            if (col.CompareTag("Sage"))
+            {
+                //flower = col.gameObject;
+                cash = true;
+                touchedFlower = flower2;
+            }
+            if (col.CompareTag("Ginseng"))
+            {
+            //flower = col.gameObject;\
+                touchedFlower = flower3;
+                cash = true;                
+            }
+    }
+    void OnTriggerExit()
+    {
+        cash = false;
+    }
+    void gettea()
+    {
+        Debug.Log("Flower Grabbed");
+        GameObject newflower = Instantiate(touchedFlower) as GameObject;
+        newflower.transform.SetParent(anchor);
+    }
+}
---
+++ Assets/Waterfill.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waterfill : MonoBehaviour {
+    public Renderer rend;
+    public bool isBoiled;
+    public int timer;
+	// Use this for initialization
+	void Start () {
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+    public void waterappear()
+    {
+        rend = gameObject.GetComponent<Renderer>();
+        Debug.Log("Water Appears");
+        rend.enabled = true;
+        
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Water"))
+        {
+            waterappear();
+        }
+
+        if (col.CompareTag("Fire"))
+        {
+            //timer 
+            Debug.Log("Boiling has begun");
+            InvokeRepeating("boiler", 1f, 1f);
+         }
+    } 
+    void boiler()
+    {
+        timer++;
+        if (timer == 3)
+    {
+        Debug.Log("Yeet");
+    }
+    }
+}
